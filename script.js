f4ca91e1dc76824d1275df5b323c670306fa4baa
danielglucksman <47191558+danielglucksman@users.noreply.github.com>
2022-12-11 19:55:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ alert('root is the cause 9');
 document.querySelectorAll('#checkout-main input[type=radio]').addEventListener("click", (element) => {
   console.log(element);
   console.log('button has been clicked');
-}).
+});
